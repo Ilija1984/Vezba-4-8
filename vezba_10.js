01d661d2a08172e774fe69e4e771cb412a24d3ad
Ilija1984 <zikicilija@hotmail.com>
2016-08-04 11:25:41 +0000

    Add files via upload

---
+++ vezba_10.js
@@ -0,0 +1,155 @@
+$(document).ready(function(){
+
+// 01
+
+// $('div').not('.wrapp').append('<div class="close">x</div>');
+
+// $('.close').click(function(){
+
+// 	$(this).parent().hide();
+
+
+// })
+
+// 02
+
+// var dd = $('dd');
+// dd.hide();
+
+// $('dt').mouseenter(function(){
+	
+// 	dd.slideUp();
+// 	$(this).next().slideDown();
+
+// })
+
+
+
+// 03
+
+// $('dt').eq(1).next().css({
+// 	color: 'red',
+// 	fontSize: '40px',
+	
+// }).html('Ilija');
+	
+//04 addBack()
+
+// $('dt').addClass('proba');
+// $('dt').next().add('div').addClass('wrapp');
+
+// $( "li.third-item" ).nextAll().addBack().css( "background-color", "red" );
+
+// $('dt').eq(0).addClass('proba1');
+// $('.proba1').nextAll().addBack().css( "background-color", "red" );
+
+// $('dt').eq(2).addClass('proba1').css( "background-color", "red" );
+// $('.proba1').addBack().css( "background-color", "red" );
+
+// 05 addClass()
+
+// $('p:last').addClass('proba');
+// $('p').last().addClass('proba');
+// $('p').first().addClass('proba');
+// $('p').first().next().addClass('proba');
+// $('p').not('p:first').nextAll().addBack().addClass('proba');
+
+// 06
+
+// $( ".inner" ).after( "<p>Test</p>" );
+
+// $('.inner').after('Ilija');
+// $('.inner').addClass('proba');
+
+// $('dt').not('dt:first').after().append('<p>Ilija</p>');
+
+//  07
+
+// $('.container').append($('h2'));
+
+// $('dt').not('.ne').append('<p>Kica</p>');
+
+
+// 08
+
+// $('dt').next().eq(3).css('color', 'red');
+
+// 09
+
+// $('dd').eq(3).after('Ilija');
+// $('dd').eq(4).html('Ilija Zikic');
+
+// 10
+
+// $('dt').not('.ne').css('color', 'red');
+// $('dt').not('.ne').after('After').css('color', 'red');
+
+// 11
+
+// $( ".target" ).change(function() {
+//   alert( "Hello" );
+// });
+
+// 12
+
+// $('button').click(function(){
+
+// 	$('.first').slideUp(1000).delay(3000).slideDown(1000);
+// 	$('.second').slideUp(2000).delay(3000).slideDown(2000);
+
+// })
+
+// 13
+
+// $('li').has('ul').css('color', 'blue');
+
+// $('dt').has('dd').css('color', 'red');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
